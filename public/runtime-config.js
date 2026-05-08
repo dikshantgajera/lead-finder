fe6826f31b44a053ebfd3c2ed84fbcbee67c1061
Dikshant Gajera <dikshantgajera@gmail.com>
2026-05-08 14:32:10 +0530

    set worker and storage

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,6 +1,6 @@
 window.__LEADFINDER_CONFIG__ = window.__LEADFINDER_CONFIG__ || {
   mode: location.hostname === 'localhost' ? 'local' : 'hosted',
-  apiBaseUrl: '',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  apiBaseUrl: 'https://leadfinder-worker.dikshantgajera.workers.dev',
+  supabaseUrl: 'https://umohhuajdhguksisncyn.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtb2hodWFqZGhndWtzaXNuY3luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjEwNjgsImV4cCI6MjA5Mzc5NzA2OH0.Yo1yv79J-5FdGwOa3smbbL5E7bOC9HtBo7Yh3w3HF94',
 };
